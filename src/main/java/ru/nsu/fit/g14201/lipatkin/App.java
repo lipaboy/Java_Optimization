16d2707b6e3f1c31791407f66046cac1b942b512
lipatkin96 <lipatkin96@gmail.com>
2017-11-19 15:03:05 +0700

    Write lab4

--- src/main/java/ru/nsu/fit/g14201/lipatkin/App.java
+++ src/main/java/ru/nsu/fit/g14201/lipatkin/App.java
@@ -4,6 +4,9 @@ import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.Loader;
+import javassist.util.proxy.Proxy;
+import javassist.util.proxy.MethodFilter;
+import javassist.util.proxy.MethodHandler;
 import javassist.util.proxy.ProxyFactory;
 
 import java.lang.reflect.Method;
@@ -16,7 +19,7 @@ import java.lang.reflect.Method;
 public class App 
 {
     public static class Calculator {
-        
+
         public int sum(int x, int y) {
             return x + y;
         }
@@ -26,36 +29,37 @@ public class App
         //todo use JavaAssist to inherit Calculator class,
         //overload sum() method
         //and add 1 to original return value, so 2 + 2 will be 5
-        return new Calculator();
+
+        ProxyFactory f = new ProxyFactory();
+        f.setSuperclass(Calculator.class);
+        f.setFilter(new MethodFilter() {
+            public boolean isHandled(Method m) {
+                return m.getName().equals("sum");
+            }
+        });
+        Class c = f.createClass();
+        MethodHandler mi = new MethodHandler() {
+            public Object invoke(Object self, Method m, Method proceed,
+                                 Object[] args) throws Throwable {
+                return (int)proceed.invoke(self, args) + 1;  // execute the original method.
+            }
+        };
+        Calculator cal = (Calculator)c.newInstance();
+        ((Proxy)cal).setHandler(mi);
+
+        return cal;
     }
 
-    public static class WrongCalculator extends Calculator {}
+//    public static class WrongCalculator extends Calculator {}
 
     public static void main( String[] args )
     {
         try {
-            ClassPool pool = ClassPool.getDefault();
-            Loader loader = new Loader(pool);
-            System.out.println(pool.insertClassPath("./target/classes"));
 
-            CtClass wrongCalcCt = pool.get(WrongCalculator.class.getName());
-            //wrongCalcCt.setSuperclass(pool.get(Calculator.class.getName()));
-            //CtMethod m = wrongCalcCt.getMethod("sum", "int, int");
-            //m.insertBefore("{ System.out.println($1); System.out.println($2); }");
-            //m.insertAfter("$_ += 1;");
-            wrongCalcCt.writeFile();
-
-//            Class wrongCalcClass = loader.loadClass(WrongCalculator.class.getName());
-//            Object wrongCalc = wrongCalcClass.newInstance();
-
-            Method toString = Class.forName(Calculator.class.getName()).getDeclaredMethod("sum");
-            System.out.println(toString);
-
-            //WrongCalculator wrongCalc = new WrongCalculator();
             Calculator cal = createCalculator();
             System.out.println("2 + 2 = " + cal.sum(2, 2));
         } catch (Exception exp) {
-            System.out.println("Exception: " + exp.getMessage());
+            System.out.println("Exception_228: " + exp.getMessage());
         }
     }
 }
